5593165272c553409f8cab870a06ec1ec441756a
Joseph Chan <joschan@yahoo.co.uk>
2024-07-02 08:26:22 +0000

    Create datebase fields

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Booklist(models.Model):
+    bookid = models.IntegerField(unique=True)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    
