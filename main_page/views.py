fceaec9e6ffa77ac7f636e80f0f06e6f68cfbcac
Joseph Chan <joschan@yahoo.co.uk>
2024-07-02 09:42:28 +0000

    Create view to display the book list

--- main_page/views.py
+++ main_page/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import booklist
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Leave Management System")
+#def index(request):
+#   return HttpResponse("Leave Management System")
+
+class PostList(generic.ListView):
+    model = booklist
--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main_page import views as index_views
+#from main_page import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("main_page.urls"), name="main_page-urls"),
     path('admin/', admin.site.urls),
 ]
