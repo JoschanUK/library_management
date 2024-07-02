fe81aa88c5e4a0f21a6f5a699a0708e9d224bf5f
Joseph Chan <joschan@yahoo.co.uk>
2024-07-02 12:15:07 +0000

    Code is working - Display all books on webpage

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(template_name="main_page/book_list.html"), name='home'),
 ]
