0e397d59dd47336c47301fe0c7710104258d93b9
Joseph Chan <joschan@yahoo.co.uk>
2024-07-02 15:15:34 +0000

    Added bok_detail view

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(template_name="main_page/index.html"), name='home'),
+    path('<slug:slug>/', views.book_detail, name='book_detail'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -12,3 +12,26 @@ class PostList(generic.ListView):
     model = booklist
     template_name = "blog/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "main_page/book_detail.html",
+        {"post": post},
+    )
