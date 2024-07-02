900eaad09246570d933d981c196adcdb70d77e76
Joseph Chan <joschan@yahoo.co.uk>
2024-07-02 09:02:52 +0000

    Create member_reocrds, comment, book_borrowed nodels

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Booklist
+from .models import booklist
+from .models import member_records
+from .models import comment
+from .models import book_borrowed
 # Register your models here.
-admin.site.register(Booklist)
+admin.site.register(booklist)
+admin.site.register(member_records)
+admin.site.register(comment)
+admin.site.register(book_borrowed)
--- main_page/models.py
+++ main_page/models.py
@@ -1,10 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Booklist(models.Model):
+class booklist(models.Model):
     bookid = models.IntegerField(unique=True)
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
+
+class member_records(models.Model):
+    userid = models.IntegerField(unique=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    mobile = models.IntegerField()
+    books_allowed = models.IntegerField()
+
+class comment(models.Model):
+    commentid = models.IntegerField(unique=True)
+    userid = models.ForeignKey(
+        member_records, on_delete=models.CASCADE
+    )
+    comments = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class book_borrowed(models.Model):
+    bookid = models.OneToOneField(booklist, on_delete=models.CASCADE)
+    userid = models.OneToOneField(member_records , on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    time_extended = models.IntegerField(default=0)
+
 
     
